19057ca963b3aa0e33766cd14d9e66131f05b9d3
Ansgar Koopmann <koopmannansgar@gmail.com>
2023-05-14 12:59:32 +0200

    Removed unnecessary requirement

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "helpers/trix_helpers.rb"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
